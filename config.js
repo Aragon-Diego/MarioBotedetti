e2f0b1367051667092f5f84104c4b9d97e953aca
Diego Alonso Aragón Villarreal <32529071+Aragon-Diego@users.noreply.github.com>
2018-08-12 14:30:06 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-  consumer_key:         'xcwuAUPANO1Kn8eOLEascDfwn',
-  consumer_secret:      'DyblZ1PamRr7viozLyCEIChXUmq0mW7HVazVHT2aPO1jdoQ3xD',
-  access_token:         '938857246380900353-WJP827gJKdlWa3fWeu3bpRl1lTWEB5L',
-  access_token_secret:  '8dwdj5ymVWQTJ2JR3vNqgTtqa8jyuWogrMMdALiooEFcG',
-}
